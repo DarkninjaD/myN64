2fbd7c16c4ef80cf9b7c37deb47485a433aa28a2
DarkninjaD <d.sauerbronn@gmail.com>
2026-01-11 00:05:28 -0600

    stub out the main menu need to figure out menu layout with buttons

---
+++ src/mainMenu/mainMenu.c
@@ -0,0 +1,8 @@
+#include <libdragon.h>
+
+void main_game_menu(surface_t disp) {
+  // menu options
+  // Draw a button
+  // loop thought the menu options per button
+  // space out the button and draw everything
+}
---
+++ src/mainMenu/mainMenu.h
@@ -0,0 +1,8 @@
+
+#ifndef MAIN_MENU_H
+#define MAIN_MENU_H
+
+/* Main menu*/
+void main_game_menu(void);
+
+#endif
