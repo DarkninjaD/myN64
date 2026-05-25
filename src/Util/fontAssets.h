1513569f13859538b375232b406bed8ad01d47d4
Dayan Sauerbronn <d.sauerbronn@gmail.com>
2026-05-25 11:54:51 -0500

    added doc to fontAssets.h

--- src/Util/fontAssets.h
+++ src/Util/fontAssets.h
@@ -1,8 +1,11 @@
 #ifndef FONT_ASSETS_H
 #define FONT_ASSETS_H
 
-// Central glossary of registered Font Slot IDs
-typedef enum {
+/**
+ * @brief The fonts used in the game via Font Slot ID to be used in
+ * libdragon font reg
+ */
+typedef enum GameFontSlot {
     FONT_SIXTYFOUR = 1,
     FONT_REVALIA = 2,
     FONT_SQUAREWAVE = 3
